c59d92c22b5992738044e938e50fdfb66c3b1fc6
nichoth <nichoth@nichoth.com>
2026-03-12 16:05:02 -0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+import { WebComponent, define } from '@substrate-system/web-component'
 import {
     toAttributes,
     type Attrs
 } from '@substrate-system/web-component/attributes'
-import { define } from '@substrate-system/web-component/util'
 
 // for docuement.querySelector
 declare global {
@@ -11,35 +11,58 @@ declare global {
     }
 }
 
-export class RadioInput extends HTMLElement {
-    static TAG = 'radio-input'
-    static observedAttributes = ['label', 'name', 'value', 'checked']
+const FORM_ATTRS = [
+    'name', 'id', 'value', 'checked', 'disabled',
+    'required', 'form', 'autofocus', 'tabindex',
+] as const
 
-    handleChange_label (oldValue:string, newValue:string) {
+export class RadioInput extends WebComponent.create('radio-input') {
+    static TAG:string = 'radio-input'
+    _forwarding = false
+
+    static get observedAttributes () {
+        return ['label', ...FORM_ATTRS]
+    }
+
+    handleChange_label (_oldValue:string, newValue:string) {
         if (newValue === null) return
         const label = this.querySelector('label')
         if (!label) return
         label.childNodes.forEach(node => {
             if (node.nodeType === Node.TEXT_NODE) {
-                node.textContent = `\n            ${newValue}\n        `
+                node.textContent =
+                    `\n            ${newValue}\n        `
             }
         })
     }
 
-    attributeChangedCallback (
+    async attributeChangedCallback (
         name:string,
         oldValue:string,
         newValue:string
     ) {
+        if (this._forwarding) return
+
+        if ((FORM_ATTRS as readonly string[]).includes(name)) {
+            const input = this.querySelector('input')
+            if (!input) return
+            this._forwarding = true
+            if (newValue === null) {
+                input.removeAttribute(name)
+            } else {
+                input.setAttribute(name, newValue)
+            }
+            this.removeAttribute(name)
+            this._forwarding = false
+            return
+        }
+
         const handler = this[`handleChange_${name}`]
         if (handler) {
             handler.call(this, oldValue, newValue)
         } else {
             this.render()
         }
-    }
-
-    disconnectedCallback () {
     }
 
     connectedCallback () {
@@ -50,16 +73,26 @@ export class RadioInput extends HTMLElement {
         const labelText = this.getAttribute('label')
         if (!labelText) return
 
-        const obj:Attrs = {}
-        for (const attr of Array.from(this.attributes)) {
-            obj[attr.name] = attr.value
+        const inputAttrs:Attrs = {}
+        for (const attr of FORM_ATTRS) {
+            if (this.hasAttribute(attr)) {
+                inputAttrs[attr] =
+                    this.getAttribute(attr) || ''
+            }
         }
 
         this.innerHTML = `<label class="radio-input">
-            <input type="radio" ${toAttributes(obj)} />
+            <input type="radio"
+                ${toAttributes(inputAttrs)} />
             ${labelText}
         </label>`
+
+        this._forwarding = true
+        for (const attr of FORM_ATTRS) {
+            this.removeAttribute(attr)
+        }
+        this._forwarding = false
     }
 }
 
-define('radio-input', RadioInput)
+define(RadioInput.TAG, RadioInput)
